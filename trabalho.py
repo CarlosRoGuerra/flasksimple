5acc3fd3e3b1d55650c42b89ce74afe77e6ba550
Carlos Roberto <crrobg2005@yahoo.com.br>
2024-09-23 00:06:45 -0300

    Commit para o pipe

--- trabalho.py
+++ trabalho.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
 db = SQLAlchemy(app)
 
+#comentando isso
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
